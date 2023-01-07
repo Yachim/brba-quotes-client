decca04c17f87b6f4ca5d8ffd63ccd7648c3faa7
Yachim <jachym.kohout@gmail.com>
2023-01-07 10:59:18 +0100

    Basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-function App() {
+export default function App() {
 	return (
 		<div className="w-screen h-screen flex justify-center items-center">
-
+			<figure className="grid grid-areas-layout grid-cols-layout grid-rows-layout gap-3">
+				<img className="grid-in-img" src="https://picsum.photos/300/400" />
+				<blockquote className="grid-in-quote">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris metus. Nunc dapibus tortor vel mi dapibus sollicitudin.
+				</blockquote>
+				<figcaption className="text-center grid-in-author">John Doe</figcaption>
+			</figure>
 		</div>
 	);
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+	    "./index.html",
+    	"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			gridTemplateAreas: {
+				layout: [
+					"img quote",
+					"author quote"
+				]
+			},
+			gridTemplateColumns: {
+				layout: "1fr 40ch"
+			},
+			gridTemplateRows: {
+				layout: "1fr 1.5rem"
+			}
+		},
+  	},
+  	plugins: [
+		require('@savvywombat/tailwindcss-grid-areas')
+	],
 }
