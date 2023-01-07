128cc218c8cfad1a705adb0dbb1bdb579aabfae5
Yachim <jachym.kohout@gmail.com>
2023-01-07 11:00:57 +0100

    Change div for main

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
 	return (
-		<div className="w-screen h-screen flex justify-center items-center">
+		<main className="w-screen h-screen flex justify-center items-center">
 			<figure className="grid grid-areas-layout grid-cols-layout grid-rows-layout gap-3">
 				<img className="grid-in-img" src="https://picsum.photos/300/400" />
 				<blockquote className="grid-in-quote">
@@ -8,6 +8,6 @@ export default function App() {
 				</blockquote>
 				<figcaption className="text-center grid-in-author">John Doe</figcaption>
 			</figure>
-		</div>
+		</main>
 	);
 }
